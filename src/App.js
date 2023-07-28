07cf528aedd27c98b5861386c10915d2db907fd7
Rishal <Rishal.mhd009@gmail.com>
2023-07-29 01:04:01 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,56 +1,56 @@
 import React, { useState } from "react";
 import Recorder from "mic-recorder-to-mp3";
-import { Box, Button, Container, Paper, Typography } from "@mui/material";
-// using material UI for component building
+import { Box, Button, Container, Grid, Paper, Typography } from "@mui/material";
 
-//initial representing of recorder at bitRate: 128
 const Mp3Recorder = new Recorder({
   bitRate: 128,
 });
 
 function App() {
-  const [audioURL, setAudioURL] = useState(""); // showing the audio element with react state
-  const [recording, setRecording] = useState(false); // recording state for button change
+  const [audioURL, setAudioURL] = useState("");
+  const [recording, setRecording] = useState(false);
 
-  //start recording function
   const startRecording = () => {
     Mp3Recorder.start().then(() => {
       setRecording(true);
     });
   };
 
-  // stop recording function
   const stopRecording = () => {
     Mp3Recorder.stop()
       .getMp3()
       .then(([buffer, blob]) => {
-        const url = URL.createObjectURL(blob); // creating url for rendering the audio
+        const url = URL.createObjectURL(blob);
         setAudioURL(url);
         setRecording(false);
       });
   };
 
   return (
-    <Container sx={{marginLeft: "500px"}}>
-      <Paper
-        elevation={3}
-        style={{ padding: "16px", margin: "16px", maxWidth: "400px" }}
-      >
-        <Typography variant="h5">Audio Recorder App</Typography>
-        <Box sx={{ marginTop: "40px" }}>
-          <Button
-            variant="contained"
-            onClick={recording ? stopRecording : startRecording}
+    <Container>
+      <Grid container justifyContent="center">
+        <Grid item xs={12} md={6}>
+          <Paper
+            elevation={3}
+            style={{ padding: "16px", margin: "16px", maxWidth: "400px" }}
           >
-            {recording ? "Stop Recording" : "Start Recording"}
-          </Button>
-        </Box>
-        {audioURL && (
-          <Box sx={{ marginTop: "40px" }}>
-            <audio src={audioURL} controls style={{ marginTop: "16px" }} />
-          </Box>
-        )}
-      </Paper>
+            <Typography variant="h5">Audio Recorder App</Typography>
+            <Box sx={{ marginTop: "40px" }}>
+              <Button
+                variant="contained"
+                onClick={recording ? stopRecording : startRecording}
+              >
+                {recording ? "Stop Recording" : "Start Recording"}
+              </Button>
+            </Box>
+            {audioURL && (
+              <Box sx={{ marginTop: "40px" }}>
+                <audio src={audioURL} controls style={{ marginTop: "16px" }} />
+              </Box>
+            )}
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
