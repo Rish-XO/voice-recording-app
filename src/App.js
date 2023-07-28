227d114d405f7effc57bf499923eff946f07033b
Rishal <Rishal.mhd009@gmail.com>
2023-07-29 00:27:13 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,34 +1,78 @@
-import React from "react";
-import { AudioRecorder } from "react-audio-voice-recorder";
+import React, { useState } from "react";
+import Recorder from "mic-recorder-to-mp3";
+
+const Mp3Recorder = new Recorder({
+  bitRate: 128,
+});
 
 function App() {
-  const addAudioElement = (blob) => {
-    // Create a URL for the audio
-    const audioUrl = URL.createObjectURL(blob);
+  const [audioURL, setAudioURL] = useState("");
 
-    // Create an <audio> element for playing the recorded audio
-    const audio = document.createElement("audio");
-    audio.src = audioUrl;
-    audio.controls = true;
+  const startRecording = () => {
+    Mp3Recorder.start().then(() => {
+      console.log("recording started");
+    });
+  };
 
-    // Append the audio element to the DOM
-    document.body.appendChild(audio);
+  const stopRecording = () => {
+    Mp3Recorder.stop().getMp3().then(([buffer, blob]) => {
+      const url = URL.createObjectURL(blob);
+      setAudioURL(url); 
+    });
   };
 
   return (
     <div>
       <h1>Audio Recorder App</h1>
-      <AudioRecorder
-        onRecordingComplete={addAudioElement}
-        audioTrackConstraints={{
-          noiseSuppression: true,
-          echoCancellation: true,
-        }}
-        downloadOnSavePress={true}
-        downloadFileExtension="mp3" // Set the download extension to "mp3"
-      />
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      {audioURL && <audio src={audioURL} controls />}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { AudioRecorder } from "react-audio-voice-recorder";
+
+// function App() {
+//   const addAudioElement = (blob) => {
+//     // Create a URL for the audio
+//     const audioUrl = URL.createObjectURL(blob);
+
+//     // Create an <audio> element for playing the recorded audio
+//     const audio = document.createElement("audio");
+//     audio.src = audioUrl;
+//     audio.controls = true;
+
+//     // Append the audio element to the DOM
+//     document.body.appendChild(audio);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Audio Recorder App</h1>
+//       <AudioRecorder
+//         onRecordingComplete={addAudioElement}
+//         audioTrackConstraints={{
+//           noiseSuppression: true,
+//           echoCancellation: true,
+//         }}
+//         downloadOnSavePress={true}
+//         downloadFileExtension="mp3" // Set the download extension to "mp3"
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
