c9669ea9e90599b7dbd62fed7771a3f034232f9d
Rishal <Rishal.mhd009@gmail.com>
2023-07-29 00:53:17 +0530

    x

--- src/App.js
+++ src/App.js
@@ -56,38 +56,3 @@ function App() {
 }
 
 export default App;
-
-// import React from "react";
-// import { AudioRecorder } from "react-audio-voice-recorder";
-
-// function App() {
-//   const addAudioElement = (blob) => {
-//     // Create a URL for the audio
-//     const audioUrl = URL.createObjectURL(blob);
-
-//     // Create an <audio> element for playing the recorded audio
-//     const audio = document.createElement("audio");
-//     audio.src = audioUrl;
-//     audio.controls = true;
-
-//     // Append the audio element to the DOM
-//     document.body.appendChild(audio);
-//   };
-
-//   return (
-//     <div>
-//       <h1>Audio Recorder App</h1>
-//       <AudioRecorder
-//         onRecordingComplete={addAudioElement}
-//         audioTrackConstraints={{
-//           noiseSuppression: true,
-//           echoCancellation: true,
-//         }}
-//         downloadOnSavePress={true}
-//         downloadFileExtension="mp3" // Set the download extension to "mp3"
-//       />
-//     </div>
-//   );
-// }
-
-// export default App;
